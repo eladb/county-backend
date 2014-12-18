d81e706523988d981905949c3dbeaa6f01aaea03
Elad Ben-Israel <elad.benisrael@gmail.com>
2014-12-18 04:33:16 +0200

    emit events through redis pub/sub

--- groups.js
+++ groups.js
@@ -1,5 +1,31 @@
 var groups = {};
 
+var redis_url = process.env.REDISCLOUD_URL || 'redis://localhost:6379';
+var redis_subscriber = require('redis-url').connect(redis_url);
+var redis_publisher = require('redis-url').connect(redis_url);
+
+redis_subscriber.on('ready', function() {
+  console.log('redis connected', redis_url);
+  redis_subscriber.psubscribe('group:*');
+  redis_subscriber.on('pmessage', function(pattern, channel, message) {
+    var msg = JSON.parse(message);
+    console.log('received a message from channel', channel, '-', msg);
+    var group_id = channel.split(':')[1];
+    var group = groups[group_id];
+    if (group) {
+      group.emit('update', msg);
+    }
+  });
+});
+
+redis_subscriber.on('error', function(err) {
+  console.error('cannot connect to redis at', redis_url, '-', err.stack);
+});
+
+function publish_to_group(group_id, obj) {
+  redis_publisher.publish('group:' + group_id, JSON.stringify(obj));
+}
+
 module.exports = function(group_id) {
   var existing_group = groups[group_id];
   if (existing_group) {
@@ -8,7 +34,7 @@ module.exports = function(group_id) {
 
   var self = Object.create(new process.EventEmitter());
   self.setMaxListeners(1000);
-  
+
   groups[group_id] = self;
 
   var counters = {};
@@ -25,12 +51,12 @@ module.exports = function(group_id) {
     // notify all subscribers
     var counter_update = {};
     counter_update[key] = current_value;
-    self.emit('update', { counters: counter_update });
+    publish_to_group(group_id, { counters: counter_update });
   };
 
   self.message = function(metadata) {
     messages.push(metadata);
-    self.emit('update', { messages: [ metadata ] });
+    publish_to_group(group_id, { messages: [ metadata ] });
   };
 
   self.get_all = function(callback) {
